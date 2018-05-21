f20ff9b84060b44ebf5f2c77e9d860d537e46396
ellycebilhorn <1845971@00-63142.local>
2018-05-21 12:25:26 -0700

    wrote comments

--- src/Settings.java
+++ src/Settings.java
@@ -10,16 +10,17 @@ public class Settings
 {
     public static void setFiles(String id)
     {
-        
+        //stores ID in settings file on the computer, backs up everything
     }
     
     public static void setFiles(String id, List files)
     {
-        
+        //backs up specific files passed 
     }
     
     public static List getFiles()
     {
+        //gets all the file names from settings file and puts it in list, if none returns null
         return null;
     }
 }
