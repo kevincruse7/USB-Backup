5811b2079979e1c5271e2ded1f650b9392f3e219
ellycebilhorn <ellyce.l.b@gmail.com>
2018-05-30 13:15:07 -0700

    worked on setFiles()

--- src/Settings.java
+++ src/Settings.java
@@ -1,11 +1,13 @@
 import java.io.File;
 import java.util.List;
-
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 /**
  * 
  * 
- * @author
- * @version
+ * @ellyce
+ * @5-30-18
  */
 public class Settings
 {
@@ -19,14 +21,35 @@ public class Settings
         return null;
     }
     
-    public static void setFiles(List<File> files)
+    public static void setFiles(List<File> files) throws IOException
     {
-        //backs up specific files passed 
+        //backs up specific file objects passed         
+        FileWriter writer = new FileWriter("settings.txt");//write to text file
+        
+        //traverse list of file objects and write each one to the settings file
+        for (File item: files)
+        {
+            writer.write(item.toString());
+        }
+        writer.close();
     }
     
-    public static List<File> getFiles()
+    public static List<File> getFiles() throws IOException
     {
         //gets all the file names from settings file and puts it in list, if none returns null
+        FileReader reader = new FileReader("settings.txt");//if file reader fails it will throw exception
+        List<String> names;
+        
+        if (reader.read() == -1)
+        {
+            return null;
+        }
+        else
+        {
+            
+        }
+        
+        
         return null;
     }
 }
